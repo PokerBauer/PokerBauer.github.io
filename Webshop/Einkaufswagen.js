72d73f6118fb46216eefffe5e38e32193d4e048b
Benni <YouWillDisappear@freenet.de>
2021-01-25 03:18:47 +0100

    var Produkte angepasst

--- Webshop/Einkaufswagen.js
+++ Webshop/Einkaufswagen.js
@@ -1,68 +1,234 @@
-let items = document.querySelectorAll(".Kaufen");
+let items = document.querySelectorAll(".Warenkorb");
 
 var Produkte = [
-    {
-        name: "Tafel-Vollmilch",
-        bild: "TafelVM",
+    { //Geschenkset
+        name: "Schokoadventskalender_in_Holzschachtel",
+        bild: "Schokoadventskalender_in_Holzschachtel",
         preis: 1,
         imWarenkorb: 0
     },
     {
-        name: "Tafel-Zartbitter",
-        bild: "Tafel-ZB",
+        name: "Schokolade_und_Wein_Geschenkkorb",
+        bild: "Schokolade_und_Wein_Geschenkkorb",
         preis: 2,
         imWarenkorb: 0
     },
     {
-        name: "Tafel-Weiß", //ß wird nicht richtig ausgegeben??
-        bild: "Tafel-W",
+        name: "Schokotorte", //ß wird nicht richtig ausgegeben??
+        bild: "Schokotorte",
         preis: 3,
         imWarenkorb: 0
     },
     {
-        name: "Pralinen-Vollmilch",
-        bild: "Pral-VM",
+        name: "Schokowerkzeuge",
+        bild: "Schokowerkzeuge",
         preis: 4,
         imWarenkorb: 0
     },
     {
-        name: "Pralinen-Zartbitter",
-        bild: "Pral-ZB",
+        name: "Trueffel_Geschenkbox",
+        bild: "Trueffel_Geschenkbox",
         preis: 5,
         imWarenkorb: 0
     },
-    {
-        name: "Pralinen-Weiß",
-        bild: "Pral-W",
+    { //Pralinen - Vollmilch
+        name: "Mix_aus_Weiss_und_Vollmilch_Dreierlei",
+        bild: "Mix_aus_Weiss_und_Vollmilch_Dreierlei",
         preis: 6,
         imWarenkorb: 0
     },
     {
-        name: "Geschenke-Vollmilch",
-        bild: "Gesch-VM",
+        name: "Mouse_gefuellte_Praline",
+        bild: "Mouse_gefuellte_Praline",
         preis: 7,
         imWarenkorb: 0
     },
     {
-        name: "Geschenke-Zartbitter",
-        bild: "Gesch-ZB",
+        name: "Praline_mit_Mouse_und_fluessiger_Schokolade",
+        bild: "Praline_mit_Mouse_und_fluessiger_Schokolade",
         preis: 8,
         imWarenkorb: 0
     },
     {
-        name: "Geschenke-Weiß",
-        bild: "Gesch-W",
+        name: "Vollmilch_Praline_Erdbeertopping",
+        bild: "Vollmilch_Praline_Erdbeertopping",
         preis: 9,
         imWarenkorb: 0
+    },
+    { //Pralinen - Weiss
+        name: "Vollmilch_Praline_mit_Nuss_Karamell_oder_Zartbitter_Topping",
+        bild: "Vollmilch_Praline_mit_Nuss_Karamell_oder_Zartbitter_Topping",
+        preis: 10,
+        imWarenkorb: 0
+    },
+    {
+        name: "Die_weissen_Berge",
+        bild: "Die_weissen_Berge",
+        preis: 11,
+        imWarenkorb: 0
+    },
+    {
+        name: "Erdbeertraum",
+        bild: "Erdbeertraum",
+        preis: 12,
+        imWarenkorb: 0
+    },
+    {
+        name: "Gefuellte_Praline_mit_Mouse_Topping",
+        bild: "Gefuellte_Praline_mit_Mouse_Topping",
+        preis: 13,
+        imWarenkorb: 0
+    },
+    {
+        name: "Kokoskugel_mit_Nussfuellung",
+        bild: "Kokoskugel_mit_Nussfuellung",
+        preis: 14,
+        imWarenkorb: 0
+    },
+    {
+        name: "Praline_mit_Roellchen",
+        bild: "Praline_mit_Roellchen",
+        preis: 15,
+        imWarenkorb: 0
+    },
+    { //Pralinen - Zartbitte
+        name: "Drei_Schokokugeln",
+        bild: "Drei_Schokokugeln",
+        preis: 16,
+        imWarenkorb: 0
+    },
+    {
+        name: "Mouse_gefuellt_mit_Erdbeerglasur",
+        bild: "Mouse_gefuellt_mit_Erdbeerglasur",
+        preis: 17,
+        imWarenkorb: 0
+    },
+    {
+        name: "Praline_mit_fluessigem_Kern",
+        bild: "Praline_mit_fluessigem_Kern",
+        preis: 18,
+        imWarenkorb: 0
+    },
+    {
+        name: "Zartbitterkugel_mit_Goldglasur",
+        bild: "Zartbitterkugel_mit_Goldglasur",
+        preis: 19,
+        imWarenkorb: 0
+    },
+    {
+        name: "Zartbitterpraline",
+        bild: "Zartbitterpraline",
+        preis: 20,
+        imWarenkorb: 0
+    },
+    { //Tafeln - Vollmilch
+        name: "Selbstgemachte_Schokolade_mit_Erdbeer_Streuseln_oder_Nuessen",
+        bild: "Selbstgemachte_Schokolade_mit_Erdbeer_Streuseln_oder_Nuessen",
+        preis: 21,
+        imWarenkorb: 0
+    },
+    {
+        name: "Tafel_mit_Tafelfuellung",
+        bild: "Tafel_mit_Tafelfuellung",
+        preis: 22,
+        imWarenkorb: 0
+    },
+    {
+        name: "Tafel_Vollmilch",
+        bild: "Tafel_Vollmilch",
+        preis: 23,
+        imWarenkorb: 0
+    },
+    {
+        name: "Vollmilchschokolade_mit_Nuessen_und_Kokos",
+        bild: "Vollmilchschokolade_mit_Nuessen_und_Kokos",
+        preis: 24,
+        imWarenkorb: 0
+    },
+    {
+        name: "Vollmilchschokolade_mit_Nuessen",
+        bild: "Vollmilchschokolade_mit_Nuessen",
+        preis: 25,
+        imWarenkorb: 0
+    },
+    { //Tafeln - Weiss
+        name: "Luftschokolade",
+        bild: "Luftschokolade",
+        preis: 26,
+        imWarenkorb: 0
+    },
+    {
+        name: "Tafeln_mit_Fruechten",
+        bild: "Tafeln_mit_Fruechten",
+        preis: 27,
+        imWarenkorb: 0
+    },
+    {
+        name: "Weiss_und_Vollmilch_mit_Erdbeer",
+        bild: "Weiss_und_Vollmilch_mit_Erdbeer",
+        preis: 28,
+        imWarenkorb: 0
+    },
+    {
+        name: "Weisse_Schokolade_mit_Nuessen",
+        bild: "Weisse_Schokolade_mit_Nuessen",
+        preis: 29,
+        imWarenkorb: 0
+    },
+    {
+        name: "Weisse_Tafel",
+        bild: "Weisse_Tafel",
+        preis: 30,
+        imWarenkorb: 0
+    },
+    { //Tafeln - Zartbitter
+        name: "Brownie_Zartbittertafel",
+        bild: "Brownie_Zartbittertafel",
+        preis: 31,
+        imWarenkorb: 0
+    },
+    {
+        name: "Chili_Schokolade",
+        bild: "Chili_Schokolade",
+        preis: 32,
+        imWarenkorb: 0
+    },
+    {
+        name: "Lindtschokolade",
+        bild: "Lindtschokolade",
+        preis: 33,
+        imWarenkorb: 0
+    },
+    {
+        name: "Zartbitterschokolade_mit_Nuessen",
+        bild: "Zartbitterschokolade_mit_Nuessen",
+        preis: 34,
+        imWarenkorb: 0
+    },
+    {
+        name: "Zartbittertafel",
+        bild: "Zartbittertafel",
+        preis: 35,
+        imWarenkorb: 0 
     } //Produkte unter diesem Muster hinzufügen
-]//bild soll der Bildname sein -> zum nuten in AusgabeEinkaufswagen(),   TODO: anpassen an richtige Preise + richtige Bildnamen und ß schreibbar machen
+]//bild soll der Bildname sein -> zum nuten in AusgabeEinkaufswagen(),   TODO: anpassen an richtige Preise
 
-for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click",() => {
-        AnzahlItems(Produkte[i]);
-        GesamtPreis(Produkte[i]);
-        AusgabeEinkaufswagen();
-    })
+var TestObProductPage = document.querySelector(".produkt"); //Erkennt ProductPage nicht
+console.log(TestObProductPage);
+if(TestObProductPage) {
+
+    console.log("TESTinIf");
+    var altImg = link.getElementsByTagName('img')[0].alt;
+    console.log(altImg);
+    /*
+    for (let i = 0; i < items.length; i++) {
+            items[i].addEventListener("click",() => {
+            AnzahlItems(Produkte[i]);
+            GesamtPreis(Produkte[i]);
+            AusgabeEinkaufswagen();
+        })
+    } 
+    */
 }
 
 function AnzahlItems(Produkte) {
