30d9fd775031d0824a95c96b2e2a1bff2f471443
Benni <YouWillDisappear@freenet.de>
2021-01-26 06:23:38 +0100

    bildpfad als weiteres Attribut für jedes Produkt + Ausgabe angepasst

--- Webshop/Einkaufswagen.js
+++ Webshop/Einkaufswagen.js
@@ -4,245 +4,280 @@ var Produkte = [
         bild: "Schokoadventskalender_in_Holzschachtel",
         preis: 1,
         imWarenkorb: 0,
-        id: 1
+        id: 1,
+        bildpfad: "Images/Geschenkset/Schokoadventskalender_in_Holzschachtel.PNG"
     },
     {
         name: "Schokolade und Wein Geschenkkorb",
         bild: "Schokolade_und_Wein_Geschenkkorb",
         preis: 2,
         imWarenkorb: 0,
-        id: 2
+        id: 2,
+        bildpfad: "Images/Geschenkset/Schokolade_und_Wein_Geschenkkorb.PNG"
     },
     {
         name: "Schokotorte",
         bild: "Schokotorte",
         preis: 3,
         imWarenkorb: 0,
-        id: 3
+        id: 3,
+        bildpfad: "Images/Geschenkset/Schokotorte.PNG"
     },
     {
         name: "Schokowerkzeuge",
         bild: "Schokowerkzeuge",
         preis: 4,
         imWarenkorb: 0,
-        id: 4
+        id: 4,
+        bildpfad: "Images/Geschenkset/Schokowerkzeuge.PNG"
     },
     {
         name: "Trueffel_Geschenkbox",
         bild: "Trueffel_Geschenkbox",
         preis: 5,
         imWarenkorb: 0,
-        id: 5
+        id: 5,
+        bildpfad: "Images/Geschenkset/Trueffel_Geschenkbox.PNG"
     },
     { //Pralinen - Vollmilch
         name: "Mix aus Weiss und Vollmilch Dreierlei",
         bild: "Mix_aus_Weiss_und_Vollmilch_Dreierlei",
         preis: 6,
         imWarenkorb: 0,
-        id: 6
+        id: 6,
+        bildpfad: "Images/Pralinen/Vollmilch/Mix_aus_Weiss_und_Vollmilch_Dreierlei.PNG"
     },
     {
         name: "Mouse gefuellte Praline",
         bild: "Mouse_gefuellte_Praline",
         preis: 7,
         imWarenkorb: 0,
-        id: 7
+        id: 7,
+        bildpfad: "Images/Pralinen/Vollmilch/Mouse_gefuellte_Praline.PNG"
     },
     {
         name: "Praline mit Mouse und fluessiger Schokolade",
         bild: "Praline_mit_Mouse_und_fluessiger_Schokolade",
         preis: 8,
         imWarenkorb: 0,
-        id: 8
+        id: 8,
+        bildpfad: "Images/Pralinen/Vollmilch/Praline_mit_Mouse_und_fluessiger_Schokolade.PNG"
     },
     {
         name: "Vollmilch Praline Erdbeertopping",
         bild: "Vollmilch_Praline_Erdbeertopping",
         preis: 9,
         imWarenkorb: 0,
-        id: 9
+        id: 9,
+        bildpfad: "Images/Pralinen/Vollmilch/Vollmilch_Praline_Erdbeertopping.PNG"
     },
-    { //Pralinen - Weiss
+    {
         name: "Vollmilch Praline mit Nuss Karamell oder Zartbitter Topping",
         bild: "Vollmilch_Praline_mit_Nuss_Karamell_oder_Zartbitter_Topping",
         preis: 10,
         imWarenkorb: 0,
-        id: 10
+        id: 10,
+        bildpfad: "Images/Pralinen/Vollmilch/Vollmilch_Praline_mit_Nuss_Karamell_oder_Zartbitter_Topping.PNG"
     },
-    {
+    {//Pralinen - Weiss
         name: "Die weissen Berge",
         bild: "Die_weissen_Berge",
         preis: 11,
         imWarenkorb: 0,
-        id: 11
+        id: 11,
+        bildpfad: "Images/Pralinen/Weiss/Die_weissen_Berge.PNG"
     },
     {
         name: "Erdbeertraum",
         bild: "Erdbeertraum",
         preis: 12,
         imWarenkorb: 0,
-        id: 12
+        id: 12,
+        bildpfad: "Images/Pralinen/Weiss/Erdbeertraum.PNG"
     },
     {
         name: "Gefuellte Praline mit Mouse Topping",
         bild: "Gefuellte_Praline_mit_Mouse_Topping",
         preis: 13,
         imWarenkorb: 0,
-        id: 13
+        id: 13,
+        bildpfad: "Images/Pralinen/Weiss/Gefuellte_Praline_mit_Mouse_Topping.PNG"
     },
     {
         name: "Kokoskugel mit Nussfuellung",
         bild: "Kokoskugel_mit_Nussfuellung",
         preis: 14,
         imWarenkorb: 0,
-        id: 14
+        id: 14,
+        bildpfad: "Images/Pralinen/Weiss/Kokoskugel_mit_Nussfuellung.PNG"
     },
     {
         name: "Praline mit Roellchen",
         bild: "Praline_mit_Roellchen",
         preis: 15,
         imWarenkorb: 0,
-        id: 15
+        id: 15,
+        bildpfad: "Images/Pralinen/Weiss/Praline_mit_Roellchen.PNG"
     },
     { //Pralinen - Zartbitte
         name: "Drei Schokokugeln",
         bild: "Drei_Schokokugeln",
         preis: 16,
         imWarenkorb: 0,
-        id: 16
+        id: 16,
+        bildpfad: "Images/Pralinen/Zartbitter/Drei_Schokokugeln.PNG"
     },
     {
         name: "Mouse gefuellt mit Erdbeerglasur",
         bild: "Mouse_gefuellt_mit_Erdbeerglasur",
         preis: 17,
         imWarenkorb: 0,
-        id: 17
+        id: 17,
+        bildpfad: "Images/Pralinen/Zartbitter/Mouse_gefuellt_mit_Erdbeerglasur.PNG"
     },
     {
         name: "Praline mit fluessigem Kern",
         bild: "Praline_mit_fluessigem_Kern",
         preis: 18,
         imWarenkorb: 0,
-        id: 18
+        id: 18,
+        bildpfad: "Images/Pralinen/Zartbitter/Praline_mit_fluessigem_Kern.PNG"
     },
     {
         name: "Zartbitterkugel mit Goldglasur",
         bild: "Zartbitterkugel_mit_Goldglasur",
         preis: 19,
         imWarenkorb: 0,
-        id: 19
+        id: 19,
+        bildpfad: "Images/Pralinen/Zartbitter/Zartbitterkugel_mit_Goldglasur.PNG"
     },
     {
         name: "Zartbitterpraline",
         bild: "Zartbitterpraline",
         preis: 20,
         imWarenkorb: 0,
-        id: 20
+        id: 20,
+        bildpfad: "Images/Pralinen/Zartbitter/Zartbitterpraline.PNG"
     },
     { //Tafeln - Vollmilch
         name: "Selbstgemachte Schokolade mit Erdbeer Streuseln oder Nuessen",
         bild: "Selbstgemachte_Schokolade_mit_Erdbeer_Streuseln_oder_Nuessen",
         preis: 21,
         imWarenkorb: 0,
-        id: 21
+        id: 21,
+        bildpfad: "Images/Tafeln/Vollmilch/Selbstgemachte_Schokolade_mit_Erdbeer_Streuseln_oder_Nuessen.PNG"
     },
     {
         name: "Tafel mit Tafelfuellung",
         bild: "Tafel_mit_Tafelfuellung",
         preis: 22,
         imWarenkorb: 0,
-        id: 22
+        id: 22,
+        bildpfad: "Images/Tafeln/Vollmilch/Tafel_mit_Tafelfuellung.PNG"
     },
     {
         name: "Tafel Vollmilch",
         bild: "Tafel_Vollmilch",
         preis: 23,
         imWarenkorb: 0,
-        id: 23
+        id: 23,
+        bildpfad: "Images/Tafeln/Vollmilch/Tafel_Vollmilch.PNG"
     },
     {
         name: "Vollmilchschokolade mit Nuessen und Kokos",
         bild: "Vollmilchschokolade_mit_Nuessen_und_Kokos",
         preis: 24,
         imWarenkorb: 0,
-        id: 24
+        id: 24,
+        bildpfad: "Images/Tafeln/Vollmilch/Vollmilchschokolade_mit_Nuessen_und_Kokos.PNG"
     },
     {
         name: "Vollmilchschokolade mit Nuessen",
         bild: "Vollmilchschokolade_mit_Nuessen",
         preis: 25,
         imWarenkorb: 0,
-        id: 25
+        id: 25,
+        bildpfad: "Images/Tafeln/Vollmilch/Vollmilchschokolade_mit_Nuessen.PNG"
     },
     { //Tafeln - Weiss
         name: "Luftschokolade",
         bild: "Luftschokolade",
         preis: 26,
         imWarenkorb: 0,
-        id: 26
+        id: 26,
+        bildpfad: "Images/Tafeln/Weiss/Luftschokolade.PNG"
     },
     {
         name: "Tafeln mit Fruechten",
         bild: "Tafeln_mit_Fruechten",
         preis: 27,
         imWarenkorb: 0,
-        id: 27
+        id: 27,
+        bildpfad: "Images/Tafeln/Weiss/Tafeln_mit_Fruechten.PNG"
     },
     {
         name: "Weiss und Vollmilch mit Erdbeer",
         bild: "Weiss_und_Vollmilch_mit_Erdbeer",
         preis: 28,
         imWarenkorb: 0,
-        id: 28
+        id: 28,
+        bildpfad: "Images/Tafeln/Weiss/Tafeln_mit_Fruechten.PNG"
     },
     {
         name: "Weisse Schokolade mit Nuessen",
         bild: "Weisse_Schokolade_mit_Nuessen",
         preis: 29,
         imWarenkorb: 0,
-        id: 29
+        id: 29,
+        bildpfad: "Images/Tafeln/Weiss/Weisse_Schokolade_mit_Nuessen.PNG"
     },
     {
         name: "Weisse Tafel",
         bild: "Weisse_Tafel",
         preis: 30,
         imWarenkorb: 0,
-        id: 30
+        id: 30,
+        bildpfad: "Images/Tafeln/Weiss/Weisse_Tafel.PNG"
     },
     { //Tafeln - Zartbitter
         name: "Brownie Zartbittertafel",
         bild: "Brownie_Zartbittertafel",
         preis: 31,
         imWarenkorb: 0,
-        id: 31
+        id: 31,
+        bildpfad: "Images/Tafeln/Zartbitter/Brownie_Zartbittertafel.PNG"
     },
     {
         name: "Chili Schokolade",
         bild: "Chili_Schokolade",
         preis: 32,
         imWarenkorb: 0,
-        id: 32
+        id: 32,
+        bildpfad: "Images/Tafeln/Zartbitter/Chili_Schokolade.PNG"
     },
     {
         name: "Lindtschokolade",
         bild: "Lindtschokolade",
         preis: 33,
         imWarenkorb: 0,
-        id: 33
+        id: 33,
+        bildpfad: "Images/Tafeln/Zartbitter/Lindtschokolade.PNG"
     },
     {
         name: "Zartbitterschokolade mit Nuessen",
         bild: "Zartbitterschokolade_mit_Nuessen",
         preis: 34,
         imWarenkorb: 0,
-        id: 34
+        id: 34,
+        bildpfad: "Images/Tafeln/Zartbitter/Zartbitterschokolade_mit_Nuessen.PNG"
     },
     {
         name: "Zartbittertafel",
         bild: "Zartbittertafel",
         preis: 35,
         imWarenkorb: 0,
-        id: 35
+        id: 35,
+        bildpfad: "Images/Tafeln/Zartbitter/Zartbittertafel.PNG"
     } //Produkte unter diesem Muster hinzufügen
 ]//bild soll der Bildname sein -> zum nuten in AusgabeEinkaufswagen(),   TODO: anpassen an richtige Preise
 
@@ -361,11 +396,12 @@ function AusgabeEinkaufswagen(){
         if(ItemsWK && InhaltWarenkorb)  { //checkt ob es die Warenkorb seite ist
             //for(var counter = 0; counter < ItemsWK.length; counter++) {
                 InhaltWarenkorb.innerHTML = "";
+                console.log(ItemsWK.bildpfad);
                         Object.values(ItemsWK).map(Item => {
                         InhaltWarenkorb.innerHTML += `
                         <div class="PrdukteWK"> 
                         <button type="Entfernen" onclick="ItemEntfernen(${Item.id})"> Produkt aus dem Warenkorb entfernen </button>
-                        <img src ="Images/Geschenkset/${Item.bild}.PNG"></img>
+                        <img src ="${Item.bildpfad}"></img>
                         ${Item.name}
                         </div>
                         <div class="Preis">${Item.preis},00 Euro</div>
@@ -468,4 +504,4 @@ function imWKcleaner() {
 
 //TODO: Button entwicklen um nur ein Item aus dem WK zu löschen
 //TODO: Items bei dem .inWarenkorb <= 0 ist aus der NodeList killen
-// ? jeden eintrag als Array speichern -> att checken -> löschen -> neu als Nodelist in LS speicher (???)
+// -> jeden eintrag als Array speichern -> att checken -> löschen -> neu als Nodelist in LS speicher (???)
